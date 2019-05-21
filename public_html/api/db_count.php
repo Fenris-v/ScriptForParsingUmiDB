09d956808e0f965cdf5d815c02f0726f1a18f7c3
Fenris-c <anatolijv236@gmail.com>
2019-05-21 18:24:56 +0300

    repair db_count.php

--- public_html/api/db_count.php
+++ public_html/api/db_count.php
@@ -9,8 +9,8 @@ $sql_count = "SELECT cms3_objects.id
 FROM cms3_objects JOIN cms3_object_images
 ON cms3_object_images.field_id = 300 && cms3_objects.id = cms3_object_images.obj_id";
 $rs_result_count = mysqli_query($link, $sql_count);
-$total_records = 0;
 if ($rs_result_count) {
+    $total_records = 1;
     while ($row2 = mysqli_fetch_row($rs_result_count)) {
         while ($row = mysqli_fetch_array($rs_result_count, MYSQLI_ASSOC)) {
             $total_records++;
